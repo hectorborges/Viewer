555a12f4b984e82a7e17ecf679582e782e70d6bf
hectorborges <hectorborges94@gmail.com>
2018-10-31 14:20:24 -0500

    revamped ui

---
+++ Assets/ObjectChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectChanger : MonoBehaviour
+{
+    public GameObject[] changeableObjects;
+    public GameObject currentObject;
+
+    public void ChangeObject(int newObjectIndex)
+    {
+        if(currentObject)
+            currentObject.SetActive(false);
+
+        currentObject = changeableObjects[newObjectIndex];
+        currentObject.SetActive(true);
+    }
+}
