3acc251c74409870c62c87151832be06ffd9936a
hectorborges <hectorborges94@gmail.com>
2018-11-07 10:31:16 -0600

    added the ability to change cabinets and fixed the material changer

---
+++ Assets/SelectArea.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectArea : MonoBehaviour
+{
+    public Color selectedColor;
+    public Color unSelectedColor;
+    public ObjectChanger objectChanger;
+    public Image currentImage;
+
+    private void Start()
+    {
+        SetCurrentImage(currentImage);
+    }
+
+    public void SelectNewArea(ObjectChanger newObjectChanger)
+    {
+        objectChanger = newObjectChanger;
+    }
+
+    public void SetCurrentImage(Image newImage)
+    {
+        if (currentImage)
+            currentImage.color = unSelectedColor;
+        currentImage = newImage;
+        currentImage.color = selectedColor;
+    }
+
+    public void ChangeOject(int newObjectIndex)
+    {
+        objectChanger.ChangeObject(newObjectIndex);
+    }
+}
